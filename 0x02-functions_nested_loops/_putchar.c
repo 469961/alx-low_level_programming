dbfe8b58ce0c18e0433587c1f65a60ff9bc5a6b7
469961 <mphomeshackslk@gmail.com>
2022-12-14 19:37:03 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,9 +4,7 @@
  
  * _putchar - writes the character c to stdout                                                       
  * @C:The character to print                                                                        
- * Return: on success 1.                                                                           
- * on error, -1 is returned, and errno is set appropriately                     
- *
+ * Return: on success 1.                                                        
  */
 
 int _putchar(char c)                                                                                    
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,21 @@
-#include "main.h"
+#ifndef MAIN_H                                                              
+#define MAIN_H
 
-/**
- * main - prints _putchar and a new line
- *Return: Always
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 
-	return (0,);
-}
+void print_alphabet(void);                                                      
+void print_alphabet_x10(void)                                          
+int _islower(int c);                                                       	int _isalpha(int c);
+int print_sign(int n);                                                         
+int _abs(int);                       
+int print_last_digit(int);
+void jack_bauer(void);                                                         
+void times_table(void);                                             
+int add(int, int)                                          
+void print_to_98(int n);                                             
+int _putchar(chat c);
+
+#endif
+
+	                                                                                                        
+
+
