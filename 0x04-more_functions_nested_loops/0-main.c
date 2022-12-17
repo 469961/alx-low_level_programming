2ca897cc187378ba8baa276dbebf60b296ec3efd
469961 <mphomeshackslk@gmail.com>
2022-12-17 08:00:59 -0800

    0. isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -12,8 +12,8 @@ int main(void)
     char c;
     
     c = 'A';
-    printf("%c: %d\n", c, _isuppert(c));
+    printf("%c: %d\n", c, _isupper(c));
     c = 'a';
-    printf("%c: %d\n", c, _isuppert(c));
+    printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _isuppert(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int _mul(int a, int b);
 void print_numbers(void);
