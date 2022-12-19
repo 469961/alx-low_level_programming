ab8df8207aadcd2724a02ba7a003acd96cc0b774
469961 <mphomeshackslk@gmail.com>
2022-12-19 09:14:04 -0800

    universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x < 14; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y < 14; y++)
 		{
 			_putchar(y + '0');
 		}
