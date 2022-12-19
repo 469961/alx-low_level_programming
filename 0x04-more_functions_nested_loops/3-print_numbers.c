172447f08ea3cc77cf0fee7df64c7938fe0f8e26
469961 <mphomeshackslk@gmail.com>
2022-12-19 07:16:41 -0800

    the numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers -> print numbers
- * Return: void
  */
 
 void print_numbers(void)
 {
-	char c;
+	int x;
 
-	for (x = 0; c <= '9'; c++)
+	for (x = 0; x <= '9'; x++)
 	{
-		_putchar(c)
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
