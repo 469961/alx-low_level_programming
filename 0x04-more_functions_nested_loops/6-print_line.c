ee604a310abe390864c649a9c94206c3314a804a
469961 <mphomeshackslk@gmail.com>
2022-12-19 17:35:37 -0800

    two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,8 +9,8 @@ void print_line(int n)
 	int x;
 
 	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x <= n; x++)
-		_putchar(95);
+	    _putchar('\n');
+	for (x = 0; x < n; x++)
+	    _putchar(95);
 	_putchar('\n');
 }
