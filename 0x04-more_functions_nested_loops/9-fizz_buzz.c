344a72d71220255b413c7a511355b0096dc3c673
469961 <mphomeshackslk@gmail.com>
2022-12-20 12:44:32 -0800

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/***
+ * main - Fizz-buzz test
+ *
+ * Return: 0 on success
+ */
+int main (void)
+{
+	int start = 1, end = 100;
+
+	while (start <= 100)
+	{
+		if (start % 3 == 0)
+			printf("Fizz");
+		if (start % 5 == 0)
+			printf("Buzz");
+		if (start % 3 != 0 && start % 5 != 0)
+			printf("%d", start);
+
+		putchar(start == end ? '\n' : ' ');
+		start++;
+	}
+
+	return (0);
+}
