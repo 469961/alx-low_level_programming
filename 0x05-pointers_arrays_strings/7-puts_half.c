71cdc61630ab3925b07b92550eff277011a5967b
469961 <mphomeshackslk@gmail.com>
2022-12-21 12:59:11 -0800

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,6 +22,7 @@ void puts_half(char *str)
 	while (half < index)
 	{
 		_putchar(str[half]);
+		half++;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 
-
 #endif
