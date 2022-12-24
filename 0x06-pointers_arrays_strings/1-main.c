369b1ee8e1a87b7334f2057e633af8ca707f6676
469961 <mphomeshackslk@gmail.com>
2022-12-24 00:00:23 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code*
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * Return: Always 0.
+ */
 
 int main(void)
-
 {
 
 	    char s1[98] = "Hello ";
