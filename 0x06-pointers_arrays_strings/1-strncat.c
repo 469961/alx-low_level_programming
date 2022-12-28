0aa938c9182f33de791ff53e99faf86ef7fbe03d
469961 <mphomeshackslk@gmail.com>
2022-12-28 01:48:17 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  *_strcat - concatenate two strings but add inputted number of bytes
- * @dest: String to be appended upon
- * @src: string to be complete to be complete at end of the dest
- * @n: integer parameter to compare index to
- * Return: return new concatenated string
+ *		an inputted number of bytes from src.
+ * @dest: String to be appended upon.
+ * @src: string to be complete to be complete at end of the dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: return new concatenated string dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
 	int index = 0, dest_len = 0;
 
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
