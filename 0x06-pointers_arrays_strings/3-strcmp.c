dd4242763652409ea226dde00983058717a42b0c
469961 <mphomeshackslk@gmail.com>
2022-12-28 09:37:30 -0800

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stcmp - Compares pointers to two strings.
+ * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
@@ -9,7 +9,7 @@
  * if str1 == str2, 0.
  * if str1 > str2, the positive difference of the first unmatched char
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
