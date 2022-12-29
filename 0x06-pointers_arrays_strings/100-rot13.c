1016d7807fff5a7217dbb2d2c0cef4d1172728d9
469961 <mphomeshackslk@gmail.com>
2022-12-29 11:09:05 -0800

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * rot13 - encoder rot13
@@ -8,21 +7,23 @@
  * Return: success
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] =
+	       	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = 
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data[j])
+			if (s[i] == data1[j])
 			{
-				s[i] = datarot[j]);
-				break
+				s[i] = datarot[j];
+				break;
 			}
 		}
 	}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"                                                                                          
+
+#include <unistd.h>                                                                                        
+
+                                                                                                           
+
+/**                                                                                                        
+ *
+ *  * putchar - write the character c to stdout                                                               
+ *
+ *   * @c: the character to print                                                                              
+ *
+ *    *                                                                                                         
+ *
+ *     * Return: on success 1                                                                                    
+ *
+ *      */                                                                                                        
+
+int _putchar(char c)                                                                                       
+
+{                                                                                                          
+
+	        return (write(1, &c, 1));                                                                          
+
+}                                                                                                          
+
+   
