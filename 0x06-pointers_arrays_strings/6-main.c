203bdfc6f69056b09a227d5fdb57b5cf8a02be50
469961 <mphomeshackslk@gmail.com>
2022-12-30 00:10:00 -0800

    Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char str[] = 
+"Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world hello world.hello world\n";
 	char *ptr;
 	
 	ptr = cap_string(str);
 	printf("%s", ptr);
 	printf("%s", str);
 	return (0);
-
 }
