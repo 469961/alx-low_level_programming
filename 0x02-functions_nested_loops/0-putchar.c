d95fc1128a933114a4f3502373cd60513248238b
469961 <mphomeshackslk@gmail.com>
2022-12-31 22:05:22 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Entry point
  * Return: Always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,7 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 
 }
