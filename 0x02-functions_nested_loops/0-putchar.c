0251b870ff12cccf2dc70f7705de4defa426df17
469961 <mphomeshackslk@gmail.com>
2022-12-31 22:33:58 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -17,5 +17,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void); 
-void print_alphabet_x10(void);                                          
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,10 +11,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int)
-void print_to_98(int n);
-int _putchar(chat c);
-
+int print_to_98(int n);
+void print_times_table(int n);
 #endif
 
 	                                                                                                        
