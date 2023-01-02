db3478246ab2e62cd4647281223f0f658c54cd82
469961 <mphomeshackslk@gmail.com>
2023-01-01 22:03:47 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,12 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int print_to_98(int n);
 void print_times_table(int n);
 int add(int, int);
+void print_to_98(int n);
+
 
 #endif
-
-	                                                                                                        
-
-
