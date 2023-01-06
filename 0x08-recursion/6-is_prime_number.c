b756bfed16f85543cd3518ba8a2fa3d32c035cfd
469961 <mphomeshackslk@gmail.com>
2023-01-06 07:36:01 -0800

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,10 @@
 int check_prime(int, int);
 /**
  * is_prime_number -> prime identifier
+ * @n: is number
+ * Return: integer
  */
+
 int is_prime_number(int n)
 {
 	return (check_prime(n, 1));
@@ -23,6 +26,6 @@ int check_prime(int n, int i)
 	if (n % i == 0 && i > 1)
 		return (0);
 	if ((n / i) < i)
-		return(1);
+		return (1);
 	return (check_prime(n, i + 1));
 }
