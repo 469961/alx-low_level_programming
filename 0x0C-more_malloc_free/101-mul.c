2d3cb81a2ff1fe9bebe0a90eda0b87fbb6d2bffb
469961 <mphomeshackslk@gmail.com>
2023-01-12 13:47:30 -0800

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,21 +8,21 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
-		if (argc != 3)
-		{ printf("Error\n");
-		exit(98); }
-		for (i = 1; i < argc; i++)
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (argv[i][j] > 57 || argv[i][j] < 48)
-				{ printf("Error\n");
-					exit(98); }
-			}
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{ printf("Error\n");
+			exit(98); }
 		}
-		mul = atol(argv[1]) * atol(argv[2]);
-		printf("%lu\n", mul);
-		return (0);
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
