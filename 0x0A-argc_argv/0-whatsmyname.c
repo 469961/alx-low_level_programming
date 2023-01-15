4fc26e7ba3206b9d9c2a41e86183282a71d3d718
469961 <mphomeshackslk@gmail.com>
2023-01-14 17:09:46 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,8 +6,11 @@
  * @argv: args
  * Return: int
  */
+
 int main(int argc, char *argv[])
+
 {
+
 unsigned long mul;
 int i, j;
 	if (argc != 3)
