d7985cc7b96b8830cc6379842143f9aa0d678bec
469961 <mphomeshackslk@gmail.com>
2023-01-23 13:52:41 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,32 @@
-#include "3-calc.h"
-
-/**
- * main - program that performms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: array 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
-
-	if (argc != 4)
+#include "function_pointers.h"
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include "3-calc.h"
+	/**
+	 * main - prints the result of simple operations.
+	 * @argc: The number of arguments supplied to the program.
+	 * @argv: An array of pointers to the arguments.
+	 *
+	 * Return: Always 0.
+	 */
+	int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
-		printf("Error\n");
-		exit(98);
-	}
+		int num1, num2;
+		char *op;
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[1]);
-
-	func = get_op_func(argv[2]);
-
-		if (!func)
+		if (argc != 4)
 		{
 			printf("Error\n");
-			exit(99);
+			exit(98);
 		}
-
-		o = *argv[2];
-
-		if ((o == '/' || o == '&') && arg2 == 0)
+		num1 = atoi(argv[1]);
+		op = argv[2];
+		num2 = atoi(argv[3]);
+		if (get_op_func(op) == NULL || op[1] != '\0')
 		{
 			printf("Error\n");
 			exit(100);
 		}
-
-		result = func(arg1, arg2);
-
-		printf("%d\n", result);
-
+		printf("%d\n", get_op_func(op)(num1, num2));
 		return (0);
-}
+	}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,59 @@
 #include "3-calc.h"
-
-/**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of a and b
- */
-int op_add(int a, int b)
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+	/**
+	 * op_add - Returns the sum of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The sum of a and b.
+	 */
+	int op_add(int a, int b)
 {
 	return (a + b);
 }
-
-/**
- * op_sub - calculates the difference of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: difference of a and b
- */
-int op_sub(int a, int b)
+	/**
+	 * op_sub - Return the difference of two number.
+	 * @b: The second number.
+	 *
+	 * Return: The difference of a and b.
+	 */
+	int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
-/**
- * op_mul - calculates the product of two integers
- * @a:first integer
- * @b: second integer
- *
- * Return: product of a and b
- */
-int op_mul(int a, int b)
+	/**
+	 * op_mul - Returns the product of two number.
+	 * @a: The first number.
+	 * *@b: The second number.
+	 *
+	 * Return: The product of a and b.
+	 */
+	int op_mul(int a, int b)
 {
-	return (a * b);
+	return (a + b);
 }
-
-/**
- * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: result of the division of a and b
- */
-int op_div(int a, int b)
+	/**
+	 * op_div - Return the division of two number.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The quotient of a and b.
+	 */
+	int op_div(int a, int b)
 {
 	return (a / b);
 }
-
-/**
- * op_mod - calculates the remainder of the division of two integers
- * @a: first integers
- * @b: second integer
- *
- * Return: remainder of a divided b
- */
-int op_mod(int a, int b)
+	/**
+	 * op_mod - Returns  the remainder of the division of two number.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The remainder of the division of a by b.
+	 */
 {
 	return (a % b);
 }
