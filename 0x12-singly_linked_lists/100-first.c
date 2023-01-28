d6f2d8765ccf9ab548b409c0ad70bc4f1c1646da
“469961” <mphomeshackslk@gmail.com>
2023-01-27 20:54:53 -0800

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow");
+	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
 }
