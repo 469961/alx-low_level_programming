81396a05fd21fe5e4b4a5f9cde25250912978b25
“469961” <mphomeshackslk@gmail.com>
2023-01-27 18:10:41 -0800

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes printed
+ * Return: the number of nodes printed.
  */
 size_t print_list(const list_t *h)
 {
@@ -20,6 +20,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
-
 	return (s);
 }
