274ae20c181a81f9761469c492f6170ddbaf36da
“469961” <mphomeshackslk@gmail.com>
2023-02-03 09:06:36 -0800

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,14 +2,14 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node to a listint_t
- * list at given position.
+ * list at a given position.
  * @head: A pointer to the address of the
  * head the listint_t list.
  * @idx: The index of the listint_t list where the new
  * node should be added - indices start at 0.
  * @n: The integer for the new node to contain.
  *
- * Return: if the function fails - NULL.
+ * Return: If the function fails - NULL.
  * otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -27,6 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = copy;
 		*head = new;
+		return (new);
 	}
 
 	for (node = 0; node < (idx - 1); node++)
