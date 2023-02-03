026d2f3808a3ef99c19a5c8de74cef568114beb2
“469961” <mphomeshackslk@gmail.com>
2023-02-03 09:16:04 -0800

    11. Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint_t - Reverses a listint_ list.
+ * reverse_listint - Reverses a listint_t list.
  * @head: A pointer to the address of
  * the head of the list_t list.
  *
