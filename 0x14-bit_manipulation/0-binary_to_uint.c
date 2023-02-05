ae771e3131e5218c16ba32dabf9886bab9b7fe57
“469961” <mphomeshackslk@gmail.com>
2023-02-05 13:30:44 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: the converted number
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
