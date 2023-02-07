e7b1cade4741163b3b944effd5bfa24a01c6a12b
“469961” <mphomeshackslk@gmail.com>
2023-02-07 13:39:35 -0800

    3-cp.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
@@ -101,4 +101,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
